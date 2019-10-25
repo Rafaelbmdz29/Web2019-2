61e9d2c070e05a3a297ce55bf8f43f6d8569b2ac
RafaelBmdz29 <rafaelbermuz@gmail.com>
2019-10-24 19:46:31 -0500

    creando formulario reactivo

--- src/app/pages/biblioteca/tipo/tipo.component.ts
+++ src/app/pages/biblioteca/tipo/tipo.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-tipo',
@@ -6,10 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tipo.component.scss']
 })
 export class TipoComponent implements OnInit {
+  public titulo = "Formulario Tipo Libro "
+public tipoForm: FormGroup; //paso 1 "tipoForm hace referencia al nombre del formulario"
 
-  constructor() { }
+//paso 2 inyeccion de dependecias
+  constructor(protected fb:FormBuilder) {
 
+   } 
   ngOnInit() {
+  }
+      //paso 3 crear metodo
+  createForm(){
+
+    this.tipoForm = this.fb.group({
+
+      idTipo: ' ' ,
+      tipoLibro: ' ',
+      descripcion: ' '
+
+    });
+
   }
 
 }
